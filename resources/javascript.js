f2c7537e9d5a32553788d8ed644bbee5c8bfc893
Scott Taylor <scotttaylor34@gmail.com>
2015-10-17 15:00:24 -0700

    external javascript file

--- resources/javascript.js
+++ resources/javascript.js
@@ -1,1 +1,8 @@
 // Your Javascript will go here!
+
+function replaceName() {
+  var myNewName = window.prompt('Pick a new name!');
+  document.getElementById('user-name').innerHTML = myNewName;
+}
+
+document.onclick = replaceName;
